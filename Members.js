bb65c0df0b304cdc52c875f25f1470f82005dbf9
vguenoff <vasil.genov@dhl.com>
2020-08-14 12:18:39 +0300

    Get members and single member

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'delectus',
+        email: 'delectus@autem.com',
+        status: 'active',
+    },
+    {
+        id: 2,
+        name: 'dolor',
+        email: 'dolor@autem.com',
+        status: 'active',
+    },
+    {
+        id: 3,
+        name: 'voluptate',
+        email: 'voluptate@autem.com',
+        status: 'active',
+    },
+    {
+        id: 4,
+        name: 'distinctio',
+        email: 'distinctio@autem.com',
+        status: 'active',
+    },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Init middleware
+app.use(logger);
+
+// Gets All Members
+app.get('/api/members/', (req, res) => res.json(members));
+
+// Gets Single Member
+app.get('/api/members/:id', (req, res) => {
+    const checkMember = member => member.id === Number(req.params.id);
+    const found = members.some(checkMember);
+
+    found
+        ? res.json(members.filter(checkMember))
+        : res
+              .status(400)
+              .json({ msg: `No member with the id of ${req.params.id}` });
+});
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
