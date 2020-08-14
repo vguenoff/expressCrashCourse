2b8ae23cb089e7d5cfd5a465eede0072295535d8
vguenoff <vasil.genov@dhl.com>
2020-08-14 10:19:33 +0300

    Static pages

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
