535ec79015f833b505f2e19a2e8b65abfc99a06b
vguenoff <vasil.genov@dhl.com>
2020-08-16 18:14:01 +0300

    Delete a member

--- routes/api/members.js
+++ routes/api/members.js
@@ -66,4 +66,22 @@ router.put('/:id', (req, res) => {
     }
 });
 
+// Delete a member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === Number(req.params.id));
+
+    if (found) {
+        res.json({
+            msg: 'Member deleted',
+            members: members.filter(
+                member => member.id !== Number(req.params.id)
+            ),
+        });
+    } else {
+        res.status(400).json({
+            msg: `No member with the id of ${req.params.id}`,
+        });
+    }
+});
+
 module.exports = router;
